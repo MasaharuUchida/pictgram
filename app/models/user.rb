1ea91fb0e89d11a62b6f1e29c7f77411b41df115
MASA_U <masaharu.uchida@gmail.com>
2019-06-15 00:04:44 +0000

    navigation bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
-# validates :passowrd, length: {in: 8..32}  #８文字以上３２文字以内
+  validates :passowrd, length: {in: 8..32}  #８文字以上３２文字以内
 # validates :password, format: {with: /^[a-zA-Z0-9]+$/} #数字とアルファベット
 # validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}　#メールアドレス
 # validates :name, length: {maximum: 15} #15文字以内
